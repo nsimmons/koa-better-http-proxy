39896a25a8e98be857cf4023ae99e89e9e4284a7
Alexander <alex@appsynth.net>
2018-05-30 12:09:37 +0700

    write tests

--- test/userResDecorator.js
+++ test/userResDecorator.js
@@ -5,11 +5,39 @@ var Koa = require('koa');
 var agent = require('supertest').agent;
 var proxy = require('../');
 
-describe('userResDecorator', function() {
+function proxyTarget(port) {
+  var other = new Koa();
+  other.use(function(ctx, next) {
+    if (ctx.request.url !== '/json') {
+      return next();
+    }
+    ctx.set('content-type', 'app,lication/json');
+    ctx.body = JSON.stringify({foo: 'bar'});
+  });
+  other.use(function(ctx) {
+    ctx.status = 200;
+    ctx.set('x-wombat-alliance', 'mammels');
+    ctx.set('x-custom-header', 'something');
+    ctx.body = 'Success';
+  });
+  return other.listen(port);
+}
+
+describe.only('userResDecorator', function() {
+  var other;
+
+  beforeEach(function() {
+    other = proxyTarget(8080);
+  });
+
+  afterEach(function() {
+    other.close();
+  });
 
   it('has access to original response', function(done) {
     var app = new Koa();
-    app.use(proxy('httpbin.org', {
+    app.use(proxy('http://localhost', {
+      port: 8080,
       userResDecorator: function(proxyRes, proxyResData) {
         assert(proxyRes.connection);
         assert(proxyRes.socket);
@@ -24,7 +52,8 @@ describe('userResDecorator', function() {
 
   it('works with promises', function(done) {
     var app = new Koa();
-    app.use(proxy('httpbin.org', {
+    app.use(proxy('http://localhost', {
+      port: 8080,
       userResDecorator: function(proxyRes, proxyResData) {
         return new Promise(function(resolve) {
           proxyResData.funkyMessage = 'oi io oo ii';
@@ -36,7 +65,7 @@ describe('userResDecorator', function() {
     }));
 
     agent(app.callback())
-    .get('/ip')
+    .get('/')
     .end(function(err, res) {
       if (err) { return done(err); }
 
@@ -48,7 +77,8 @@ describe('userResDecorator', function() {
 
   it('can modify the response data', function(done) {
     var app = new Koa();
-    app.use(proxy('httpbin.org', {
+    app.use(proxy('http://localhost', {
+      port: 8080,
       userResDecorator: function(proxyRes, proxyResData) {
         proxyResData = JSON.parse(proxyResData.toString('utf8'));
         proxyResData.intercepted = true;
@@ -57,7 +87,7 @@ describe('userResDecorator', function() {
     }));
 
     agent(app.callback())
-    .get('/ip')
+    .get('/json')
     .end(function(err, res) {
       if (err) { return done(err); }
 
@@ -66,14 +96,54 @@ describe('userResDecorator', function() {
     });
   });
 
-
-  it('can modify the response headers, [deviant case, supported by pass-by-reference atm]', function(done) {
+  it('can filter response headers', function(done) {
+    var proxiedApp = new Koa();
     var app = new Koa();
-    app.use(proxy('httpbin.org', {
-      userResDecorator: function(rsp, data, ctx) {
-        ctx.set('x-wombat-alliance', 'mammels');
-        ctx.set('content-type', 'wiki/wiki');
-        return data;
+    var p1Done, p2Done;
+    var p1 = new Promise(function(resolve) { p1Done = resolve; });
+    var p2 = new Promise(function(resolve) { p2Done = resolve; });
+    app.use(proxy('http://localhost', {
+      port: 8080
+    }));
+    proxiedApp.use(proxy('http://localhost', {
+      port: 8080,
+      strippedHeaders: ['x-wombat-alliance', 'x-custom-header']
+    }));
+
+    agent(app.callback())
+    .get('/')
+    .end(function(err, res) {
+      if (err) { return done(err); }
+      assert(typeof res.headers['x-custom-header'] === 'string');
+      assert(typeof res.headers['x-wombat-alliance'] === 'string');
+      p1Done();
+    });
+
+    agent(proxiedApp.callback())
+    .get('/')
+    .end(function(err, res) {
+      if (err) { return done(err); }
+      assert(typeof res.headers['x-custom-header'] !== 'string');
+      assert(typeof res.headers['x-wombat-alliance'] !== 'string');
+      p2Done();
+    });
+
+    Promise.all([p1, p2]).then(function() { done(); });
+  });
+
+  it('can modify the response headers', function(done) {
+    var app = new Koa();
+    app.use(proxy('http://localhost', {
+      port: 8080,
+      userResHeadersDecorator: function(headers) {
+        var newHeaders = Object.keys(headers)
+          .reduce(function(result, key) {
+            result[key] = headers[key];
+            return result;
+          }, {});
+        newHeaders['x-transaction-id'] = '12345';
+        newHeaders['x-entity-id'] = 'abcdef';
+        return newHeaders;
       }
     }));
 
@@ -81,24 +151,25 @@ describe('userResDecorator', function() {
     .get('/ip')
     .end(function(err, res) {
       if (err) { return done(err); }
-      assert(res.headers['content-type'] === 'wiki/wiki');
-      assert(res.headers['x-wombat-alliance'] === 'mammels');
+      assert(res.headers['x-transaction-id'] === '12345');
+      assert(res.headers['x-entity-id'] === 'abcdef');
       done();
     });
   });
 
   it('can mutuate an html response', function(done) {
     var app = new Koa();
-    app.use(proxy('httpbin.org', {
+    app.use(proxy('http://localhost', {
+      port: 8080,
       userResDecorator: function(rsp, data) {
-        data = data.toString().replace('Oh', '<strong>Hey</strong>');
+        data = data.toString().replace('Success', '<strong>Hey</strong>');
         assert(data !== '');
         return data;
       }
     }));
 
     agent(app.callback())
-    .get('/html')
+    .get('/')
     .end(function(err, res) {
       if (err) { return done(err); }
       assert(res.text.indexOf('<strong>Hey</strong>') > -1);
